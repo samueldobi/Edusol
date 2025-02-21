d70a0c5cd47a77d23eb885edf54dd430f3325a70
Mike <michaelokolo62@yahoo.com>
2025-02-21 23:47:09 +0000

    completed the profile security setting form

---
+++ parent-web-app/app/auth/profile-security/page.tsx
@@ -0,0 +1,4 @@
+import Form from '@/app/ui/Auth/profile-security';
+export default function Page() {
+  return <Form />;
+}
---
+++ parent-web-app/app/ui/Auth/profile-security.tsx
@@ -0,0 +1,62 @@
+import { Cog8ToothIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
+
+export default function ProfileSecurityForm() {
+  return (
+    <div className="bg-white rounded-lg shadow-lg w-[40%] max-w-[500px] min-w-[320px] flex flex-col p-6 sm:w-[90%] sm:p-20 md:w-[85%] lg:w-[60%] xl:w-[35%]">
+      {/*Logo section*/}
+      <div className="flex justify-center items-center">
+        <Cog8ToothIcon className="h-20 w-20 text-indigo-950" />
+      </div>
+      {/*Title and Subtitle*/}
+      <h2 className="text-2xl font-bold w-full  mt-3 text-center text-lime-500">
+        Profile security setting
+      </h2>
+      <p className="text-sm mt-2 text-center font-light text-indigo-950">
+        create and set all your personal information at one end
+      </p>
+      {/*Form section*/}
+      <div className="mt-4 w-full flex flex-col items-stretch">
+        <form
+          action="#"
+          method="POST"
+          className="w-full  flex flex-col items-stretch"
+        >
+          {/*Submit button*/}
+          <div className="flex justify-center">
+            <button
+              className="bg-indigo-950 font-bold w-full text-white px-20 py-3"
+              type="submit"
+            >
+              Password settings
+            </button>
+          </div>
+          <div className="flex justify-center mt-5">
+            <button
+              className="bg-indigo-950 font-bold w-full text-white px-20 py-3"
+              type="submit"
+            >
+              2FA security settings
+            </button>
+          </div>
+          <div className="flex justify-center mt-5">
+            <button
+              className="bg-indigo-950 font-bold w-full text-white px-20 py-3"
+              type="submit"
+            >
+              HELP
+            </button>
+          </div>
+        </form>
+        <div className="mt-5 flex justify-center">
+          <Link
+            href="#"
+            className="font-normal mt-8 text-sm text-indigo-950 cursor-pointer hover:underline focus:underline"
+          >
+            Back to log in page
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
