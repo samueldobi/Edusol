739de9d3fd8e4506f157e8d273ac37af1f1ae89a
Mike <michaelokolo62@yahoo.com>
2025-02-21 23:19:09 +0000

    Completed the two factor authentication more

---
+++ parent-web-app/app/auth/two-factor-more/page.tsx
@@ -0,0 +1,4 @@
+import Form from '@/app/ui/Auth/two-factor-form-more';
+export default function Page() {
+  return <Form />;
+}
---
+++ parent-web-app/app/ui/Auth/two-factor-form-more.tsx
@@ -0,0 +1,72 @@
+import { LockClosedIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
+
+export default function TwoFactorFormMore() {
+  return (
+    <div className="bg-white rounded-lg shadow-lg w-[40%] max-w-[500px] min-w-[320px] flex flex-col p-6 sm:w-[90%] sm:p-11 md:w-[85%] lg:w-[60%] xl:w-[35%]">
+      {/*Logo section*/}
+      <div className="flex justify-center items-center">
+        <img alt="company logo" src="/logo.png" className="w-24 h-24" />
+      </div>
+      {/*Title and Subtitle*/}
+      <h2 className="text-2xl font-bold w-full  mt-5 text-center text-lime-500">
+        One more step
+      </h2>
+      <p className="text-sm mt-0 text-center font-light text-indigo-950">
+        Type in your two factor authentication code
+      </p>
+
+      <div className="flex justify-center items-center">
+        <LockClosedIcon className="h-20 w-20 text-indigo-950" />
+      </div>
+      {/*Form section*/}
+      <div className="mt-4 w-full flex flex-col items-stretch">
+        <form action="#" method="POST" className="w-full">
+          <div className="flex sm:mx-9 justify-between">
+            <input
+              type="text"
+              className="w-12 h-12 border-2 border-gray-300 text-xl text-center rounded-md focus:outline-none focus:ring-2 focus:ring-[#66cc00] focus:border-[#66cc00]"
+            />
+            <input
+              type="text"
+              className="w-12 h-12 border-2 border-gray-300 text-xl text-center rounded-md focus:outline-none focus:ring-2 focus:ring-[#66cc00] focus:border-[#66cc00]"
+            />
+            <input
+              type="text"
+              className="w-12 h-12 border-2 border-gray-300 text-xl text-center rounded-md focus:outline-none focus:ring-2 focus:ring-[#66cc00] focus:border-[#66cc00]"
+            />
+            <input
+              type="text"
+              className="w-12 h-12 border-2 border-gray-300 text-xl text-center rounded-md focus:outline-none focus:ring-2 focus:ring-[#66cc00] focus:border-[#66cc00]"
+            />
+            <input
+              type="text"
+              className="w-12 h-12 border-2 border-gray-300 text-xl text-center rounded-md focus:outline-none focus:ring-2 focus:ring-[#66cc00] focus:border-[#66cc00]"
+            />
+            <input
+              type="text"
+              className="w-12 h-12 border-2 border-gray-300 text-xl text-center rounded-md focus:outline-none focus:ring-2 focus:ring-[#66cc00] focus:border-[#66cc00]"
+            />
+          </div>
+          {/*Submit button*/}
+          <div className="flex justify-center mt-14">
+            <button
+              className="bg-indigo-950 font-bold text-white px-20 py-3"
+              type="submit"
+            >
+              LOG IN
+            </button>
+          </div>
+        </form>
+        <div className="mt-2 flex justify-center">
+          <Link
+            href="#"
+            className="font-normal mt-8 text-sm text-indigo-950 cursor-pointer hover:underline focus:underline"
+          >
+            Back to log in page
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
