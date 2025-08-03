b153563c91883ddc3b42bf77a20383a408c4d332
samueldob <frankdobisamuel@gmail.com>
2025-08-03 23:14:42 +0100

    add: edited nextconfig file to display images on livehost

--- parent-web-app/next.config.ts
+++ parent-web-app/next.config.ts
@@ -2,12 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+      images: {
+        domains: ['edusol-psi.vercel.app'], 
+      },
   // My own config options
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://school-service-ms-app.educesol.com/:path*',
+        destination: 'https://api-gateway-ms-app.fly.dev/:path*',
       },
     ];
   },
