b4b2766d00f0262ee10ad8e52a674914e6a47700
samueldob <frankdobisamuel@gmail.com>
2025-08-04 08:35:20 +0100

    test:testing config fixed

--- parent-web-app/next.config.ts
+++ parent-web-app/next.config.ts
@@ -1,19 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-      images: {
-        domains: ['edusol-psi.vercel.app'], 
-      },
-  // My own config options
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://api-gateway-ms-app.fly.dev/:path*',
-      },
-    ];
-  },
+
+  //     images: {
+  //       domains: ['edusol-psi.vercel.app'], 
+  //     },
+  
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://api-gateway-ms-app.fly.dev/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
